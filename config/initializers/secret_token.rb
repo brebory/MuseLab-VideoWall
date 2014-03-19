24523c0d5d668c2130de73849ca005a14fab7e94
Brendon Roberto <brendon@brendonroberto.com>
2014-03-19 16:55:21 -0400

    Initial commit, set up rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MuseLabVideowall::Application.config.secret_token = '09b23198c67c45bd7daddf05ad0597beaf86d6d56379d6c3cb55764162f6d24d0805a539e614aea529f52e6d58f046ddc4dbad4e0b6f06914d687d748a19f275'
